5eed5a737238fd4dc677b0b02a50fb869f39ca37
AliMohajeri10 <10.ali.mohajeri@gmail.com>
2026-07-18 23:37:53 +0330

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   // Correct for a repository named YOUR-USERNAME.github.io
-  base: "https://AliMohajeri.github.io/",
+  base: "/",
 });
