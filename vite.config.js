4669a09df9b0f272e83562f48ec7260702c675d4
AliMohajeri10 <10.ali.mohajeri@gmail.com>
2026-07-18 21:52:32 +0330

    Create engineering portfolio

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   // Correct for a repository named YOUR-USERNAME.github.io
-  base: "https://
+  base: "https://AliMohajeri.github.io/",
+});
